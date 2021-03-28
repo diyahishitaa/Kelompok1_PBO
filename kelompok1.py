df831b02ce8152d940615bcc1a7f8324dca05f2d
Your Name <you@example.com>
2021-03-28 16:32:11 +0700

    upload tugas 1

---
+++ kelompok1.py
@@ -0,0 +1,10 @@
+def balok(p,l,t):
+    return p * l * t
+p= 12
+l= 3
+t= 67
+volume= balok(p,l,t)
+print("panjang: ",p)
+print("lebar: ",l)
+print("tinggi: ",t)
+print("volume: ",p,"*",l,"*",t,"=",volume)
